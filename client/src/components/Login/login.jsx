1355c6e92b1a948abd032fe2a3a07fda93cee22c
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-07-24 23:53:35 -0300

    improved login styles

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -6,9 +6,9 @@ export default function Login () {
 
 
     return (
-        <>
+        <div className="loggin_wrapper">
             <img src={banner} alt="login_test_img" className="banner_img"/>
             <LoginForm />
-        </>
+        </div>
     )
 }
